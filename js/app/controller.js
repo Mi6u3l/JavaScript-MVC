d7634a86cbceeeb31bdc330c9ae967dd433eb5ce
Mi6u3l <miguelbragagomes@gmail.com>
2017-08-14 16:09:36 +0100

    add comments

--- js/app/controller.js
+++ js/app/controller.js
@@ -1,14 +1,17 @@
 define(() => {
     class Controller {
+        // initialize controller
         constructor(model, view) {
             this.model = model;
             this.view = view;
         }
 
+        // initialize received model
         initModel() {
             this.model.init();
         }
 
+        // call render from attached view
         renderView() {
             this.initModel();
             this.view.render();
--- js/app/model.js
+++ js/app/model.js
@@ -1,12 +1,14 @@
 define(() => {
     const SERVICE_URL = 'http://localhost:3000';
     class Model {
+        // initialize model class
         constructor(type, tabletColor, smartPhoneColor) {
             this.tabletColor = tabletColor;
             this.smartPhoneColor = smartPhoneColor;
             this.type = type;
         }
 
+        // get device values from db schema through service invocation
         getDeviceValues() {
             const xmlHttp = new XMLHttpRequest();
             xmlHttp.open('GET', `${SERVICE_URL}/${this.type}`, false);
@@ -15,15 +17,18 @@ define(() => {
             this.smartPhoneValue = JSON.parse(xmlHttp.responseText).smartPhone;
         }
 
+        // calculate device totals
         getTotal() {
             this.total = this.tabletValue + this.smartPhoneValue;
         }
 
+        // calculate device percentages
         getPercentages() {
             this.smartPhonePercentage = (this.smartPhoneValue * 100) / this.total;
             this.tabletPercentage = (this.tabletValue * 100) / this.total;
         }
 
+        // initialize devices values
         init() {
             this.getDeviceValues();
             this.getTotal();
