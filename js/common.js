0897ddcb029b8b1a47216dd31b0a3a83089cacd7
Mi6u3l <miguelbragagomes@gmail.com>
2017-08-15 16:03:05 +0100

    clean up

--- js/common.js
+++ js/common.js
@@ -3,7 +3,5 @@ requirejs.config({
     paths: {
         app: '../app',
         d3: '../js/lib/d3.min',
-        XMLHttpRequest: '../js/lib/XMLHttpRequest',
-        url: '../js/lib/url',
     },
 });
